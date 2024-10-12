eb9af4f45150de50df134cb72aeee6235bf84d4c
Amjed-10 <fake1987624@gmail.com>
2024-10-12 12:18:08 +0300

    Update db_connect1.php

--- Web_chatbot/db_connect1.php
+++ Web_chatbot/db_connect1.php
@@ -1,18 +1,15 @@
 <?php
-// إعداد تفاصيل الاتصال بقاعدة البيانات
-$host = 'localhost'; // اسم المضيف
-$dbUser = 'root'; // اسم مستخدم قاعدة البيانات
-$dbPass = ''; // كلمة المرور (اتركه فارغًا إذا لم يكن هناك كلمة مرور)
-$dbName = 'attendance_db'; // اسم قاعدة البيانات
+$host = 'localhost'; 
+$dbUser = 'root'; 
+$dbPass = ''; 
+$dbName = 'attendance_db'; 
 
-// إنشاء الاتصال بقاعدة البيانات
 $mysqli = new mysqli($host, $dbUser, $dbPass, $dbName);
 
-// التحقق من نجاح الاتصال
 if ($mysqli->connect_error) {
     die("Connection failed: " . $mysqli->connect_error);
 }
 
-// تحديد قاعدة البيانات `attendance_db`
+`attendance_db`
 $mysqli->select_db($dbName);
 ?>
