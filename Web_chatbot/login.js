65dabe2b7259f2964c3d83e36f377f7d67b6b069
Amjed-10 <fake1987624@gmail.com>
2024-10-12 12:21:57 +0300

    Update login.js

--- Web_chatbot/login.js
+++ Web_chatbot/login.js
@@ -1,13 +1,12 @@
 document.getElementById('loginForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent default form submission
+    event.preventDefault(); 
 
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
 
-    // Simulate a login check (replace with actual authentication logic)
     if (username === 'admin' && password === 'password') {
         localStorage.setItem('loggedIn', 'true');
-        window.location.href = 'index.html'; // Redirect to your home page URL
+        window.location.href = 'index.html'; 
     } else {
         document.getElementById('error-message').textContent = 'Invalid username or password.';
     }
